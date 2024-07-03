fcfd95aa89e47ee2a62a7ba2267cfce3c376d3dd
Gab <Gab@LAPTOP-4MOABK8P>
2024-07-03 23:21:41 +0800

    Commit

---
+++ src/crud_app/CRUD_APP.java
@@ -0,0 +1,17 @@
+
+package crud_app;
+
+import com.login.LoginFrame;
+
+public class CRUD_APP {
+   
+ 
+    public static void main(String[] args) {
+       LoginFrame loginObj = new LoginFrame();
+       loginObj.show();
+        
+      
+       
+    }
+    
+}
